f5e58579ae9b88db3feb7254cc0b473f49f92560
lvze <lvze.lv@alibaba-inc.com>
2021-04-30 16:36:23 +0800

    打包

--- scripts/build.js
+++ scripts/build.js
@@ -3,6 +3,8 @@
 // Do this as the first thing so that any code reading it knows the right env.
 process.env.BABEL_ENV = 'production';
 process.env.NODE_ENV = 'production';
+process.env.BUILD_PATH = 'docs';
+process.env.PUBLIC_URL = '/pool-h5';
 
 // Makes the script crash on unhandled rejections instead of silently
 // ignoring them. In the future, promise rejections that are not handled will
