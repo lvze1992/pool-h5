8146dcd97d9b67870067879aa7b5a431c0aaefa5
lvze <lvze.lv@alibaba-inc.com>
2021-05-02 03:04:40 +0800

    loading

--- src/components/Loading/index.js
+++ src/components/Loading/index.js
@@ -1,3 +1,10 @@
-export default function Loading() {
-  return <div>loading...</div>;
+import './Loading.scss';
+export default function Loading(props) {
+  const { label } = props;
+  return (
+    <div className="loading">
+      <div className="loader-16"></div>
+      <p>{label}</p>
+    </div>
+  );
 }
