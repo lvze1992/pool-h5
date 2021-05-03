bf54d5931d4753e48fc85d1aaae11e76b9866e5d
lvze <lvze.lv@alibaba-inc.com>
2021-05-04 01:10:45 +0800

    退出登录

--- src/pages/Setting/index.js
+++ src/pages/Setting/index.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import { useHistory } from 'react-router-dom';
+import { Button } from 'antd-mobile';
 import { CustomNav } from 'src/components';
+import { useStore } from 'src/Provider';
+
 import './Setting.scss';
 const user = {
   phone: '3434222',
 };
 export default function Setting() {
   const history = useHistory();
+  let store = useStore();
   const { phone } = user;
   return (
     <div className="setting-page">
@@ -37,6 +41,15 @@ export default function Setting() {
         <span>修改登录密码</span>
         <i className="iconfont icon-right" />
       </div>
+      <Button
+        type="ghost"
+        size="small"
+        onClick={() => {
+          store.signout();
+        }}
+      >
+        退出登录
+      </Button>
     </div>
   );
 }
